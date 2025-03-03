4d42a5ec1ab489a79afc3a55e3af41c1358ea724
taffazzel <tafaz_hossain@yahoo.com>
2025-03-03 17:29:32 +0000

    Interim Push!!

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send Message')
 
 @app.route('/submit_form', methods=['POST'])
 def submit_form():
-    name = request.form['name']
-    email = request.form['email']
-    message = request.form['message']
-    # Add your email sending logic here
-    return redirect(url_for('thank_you'))
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+        # Add your email sending logic here
+        return redirect(url_for('thank_you'))
+    return render_template('contact.html', form=form)
 
 @app.route('/thank_you')
 def thank_you():
